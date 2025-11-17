fcca470c142c86cfa0649eef8780504f0165007b
camilisima <camila.lewis1@gmail.com>
2025-11-17 19:38:49 -0300

    arreglo comentariosrutas.js

--- src/rutas/comentariosRutas.js
+++ src/rutas/comentariosRutas.js
@@ -14,7 +14,7 @@ const router = express.Router();
 
 /**
  * @swagger
- * api/comentarios:
+ * /api/comentarios:
  *   get:
  *     summary: Obtener todos los comentarios activos
  *     tags: [Comentarios]
@@ -33,7 +33,7 @@ router.get(
 
 /**
  * @swagger
- * api/comentarios/reserva/{reserva_id}:
+ * /api/comentarios/reserva/{reserva_id}:
  *   get:
  *     summary: Obtener comentarios por reserva
  *     tags: [Comentarios]
@@ -58,7 +58,7 @@ router.get(
 
 /**
  * @swagger
- * api/comentarios:
+ * /api/comentarios:
  *   post:
  *     summary: Crear un comentario sobre una reserva
  *     tags: [Comentarios]
@@ -92,7 +92,7 @@ router.post(
 
 /**
  * @swagger
- * api/comentarios/{id}:
+ * /api/comentarios/{id}:
  *   delete:
  *     summary: Borrado lógico de un comentario
  *     tags: [Comentarios]
